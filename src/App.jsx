42a125f58670d5f5daf32bd24876e3dd58a4214e
ajit waman <ajitwaman353@gmail.com>
2025-01-10 16:23:23 +0530

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./component/Navbar";
 import Skills from "./component/Skills";
-import Footer from "./component/footer";
+
+import Footer from "./component/Footer";
 import { About } from "./component/About";
 import Projects from "./component/Projects";
 import Contact from "./component/Contact";
+
 export default function () {
   return (
     <div className="min-h-screen bg-gray-900">
