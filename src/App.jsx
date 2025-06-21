02317146af5edf0e04f26b3ec90ecd5f64ab5f1f
ajit waman <ajitwaman353@gmail.com>
2025-06-21 16:28:43 +0530

    styling change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import { About } from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
